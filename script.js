a3855267c9b8371209a9e51c16fd9bd29b5f3dbe
swanie21 <kswanie21@gmail.com>
2016-07-06 19:24:45 -0600

    phase two

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ var randomGuess = document.getElementById('numberGuess');
 var guessButton = document.querySelector('.guess');
 
 function submitGuess() {
+  document.querySelector('.reset').disabled = false;
   document.getElementById('instruction').innerText = 'Your previous guess was...';
   if (parseInt(randomGuess.value) === randomNumber) {
     return document.getElementById('message').innerText = 'You are correct!';
-  } else if (parseInt(randomGuess.value) < 1 || parseInt(randomGuess.value) > 100 || isNaN(parseInt(randomGuess.value))) {
+  }
+  if (parseInt(randomGuess.value) > randomNumber) {
+   document.getElementById('message').innerText = 'Sorry, that guess is too high. Try a lower number.';
+    return document.getElementById('numberGuess').value='';
+  }
+  if (parseInt(randomGuess.value) < randomNumber) {
+    document.getElementById('message').innerText = 'Sorry, that guess is too low. Try a higher number.';
+    return document.getElementById('numberGuess').value='';
+  }
+  if (parseInt(randomGuess.value) < 1 || parseInt(randomGuess.value) > 100 || isNaN(parseInt(randomGuess.value))) {
     document.getElementById('instruction').innerText = 'This number is not within range';
     return document.getElementById('message').innerText = 'Please choose a valid number!';
-  } else if (parseInt(randomGuess.value) > randomNumber) {
-    document.getElementById('message').innerText = 'Sorry, that guess is too high. Try a lower number.';
-    return document.getElementById(numberGuess).value='';
-  } else if (parseInt(randomGuess.value) < randomNumber) {
-    return document.getElementById('message').innerText = 'Sorry, that guess is too low. Try a higher number.';
-    return document.getElementById(numberGuess).value='';
-  }
+}
 }
 
 guessButton.addEventListener('click', function () {
@@ -24,16 +28,18 @@ guessButton.addEventListener('click', function () {
     submitGuess();
 });
 
-//
-//disable buttons
-//document.querySelector('.reset').disabled = true;
-//if (document.getElementById)
-//document.querySelector('.clear').disabled = true;
-//
+//enable clear button
+function enableBtn () {
+if (document.getElementById('numberGuess') != null) {
+    return document.querySelector('.clear').disabled = false;
+}
+};
+
 //clear function
 function clearNumber () {
 document.getElementById('numberGuess').value = '';
 }
+
 //
 //reset function: clear number, make new randomNumber, clear message (.innerText)
 function resetNumber() {
@@ -41,5 +47,7 @@ document.getElementById('instruction').innerText = 'Make a guess between 1 to 10
 document.getElementById('message').innerText = '';
 document.getElementById('numberGuess').value = '';
 document.getElementById('previousUserGuess').innerText='';
+document.querySelector('.clear').disabled = true;
+document.querySelector('.reset').disabled = true;
 randomNumber = Math.floor((Math.random() * 100) + 1);
 }
