766bc3f312a8239b6b8224874d39c7202af4f315
swanie21 <kswanie21@gmail.com>
2016-07-07 19:18:04 -0600

    partial phase three

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var randomNumber = Math.floor((Math.random() * 100) + 1);
 var randomGuess = document.getElementById('numberGuess');
 var guessButton = document.querySelector('.guess');
+var min = parseInt(document.getElementById('userMinNumber').value) || 1;
+var max = parseInt(document.getElementById('userMaxNumber').value) || 100;
+
+function randomNumberVariable () {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ var randomNumber = randomNumberVariable ();
+// function randomNumberVariable (min, max) {
+//   return Math.floor(Math.random() * (max - min +1)) + min;
+// }
 
 guessButton.addEventListener('click', function () {
     previousUserGuess.innerText = randomGuess.value;
@@ -14,7 +23,7 @@ function submitGuess() {
   if (parseInt(randomGuess.value) === randomNumber) {
     return document.getElementById('message').innerText = 'You are brilliant!';
   }
-  if (parseInt(randomGuess.value) < 1 || parseInt(randomGuess.value) > 100 || isNaN(parseInt(randomGuess.value))) {
+  if (parseInt(randomGuess.value) < min || parseInt(randomGuess.value) > max || isNaN(parseInt(randomGuess.value))) {
     document.getElementById('instruction').innerText = 'This number is not within range';
     return document.getElementById('message').innerText = 'Please choose a valid number!';
   }
@@ -41,14 +50,17 @@ document.getElementById('numberGuess').value = '';
 document.getElementById('previousUserGuess').innerText='';
 document.querySelector('.clear').disabled = true;
 document.querySelector('.reset').disabled = true;
-randomNumber = Math.floor((Math.random() * 100) + 1);
+randomNumberVariable();
 }
 
 function submitUserNumbers() {
-parseInt(document.getElementById('userMinNumber').value);
-parseInt(document.getElementById('userMaxNumber').value);
-};
-
-//submit button needs to check that the min is less than the max
-
-//loop to add 10 to userMaxNumberax and subtract 10 to userMinNumber
+  document.getElementById('instruction').innerText = 'Make a guess between your selected numbers';
+  document.getElementById('message').innerText = '';
+  document.getElementById('numberGuess').value = '';
+  document.getElementById('previousUserGuess').innerText = '';
+  document.querySelector('.clear').disabled = true;
+  document.querySelector('.reset').disabled = true;
+  min = parseInt(document.getElementById('userMinNumber').value) || 1;
+  max = parseInt(document.getElementById('userMaxNumber').value) || 100;
+  randomNumber = randomNumberVariable();
+}
