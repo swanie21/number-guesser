caa6413fb502a0483a12f3a77844e4d0637a33a4
Adam Bernard <ab255@me.com>
2016-07-07 19:01:33 -0600

    minmaxworks

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-var randomNumber = Math.floor((Math.random() * 100) + 1);
 var randomGuess = document.getElementById('numberGuess');
 var guessButton = document.querySelector('.guess');
-var setUserNumbers = document.querySelector('.userNumbers');
+var min = parseInt(document.getElementById('userMinNumber').value) || 1;
+var max = parseInt(document.getElementById('userMaxNumber').value) || 100;
+
+function randomNumberVariable () {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ var randomNumber = randomNumberVariable ();
+// function randomNumberVariable (min, max) {
+//   return Math.floor(Math.random() * (max - min +1)) + min;
+// }
 
 guessButton.addEventListener('click', function () {
     previousUserGuess.innerText = randomGuess.value;
@@ -15,7 +23,7 @@ function submitGuess() {
   if (parseInt(randomGuess.value) === randomNumber) {
     return document.getElementById('message').innerText = 'You are brilliant!';
   }
-  if (parseInt(randomGuess.value) < 1 || parseInt(randomGuess.value) > 100 || isNaN(parseInt(randomGuess.value))) {
+  if (parseInt(randomGuess.value) < min || parseInt(randomGuess.value) > max || isNaN(parseInt(randomGuess.value))) {
     document.getElementById('instruction').innerText = 'This number is not within range';
     return document.getElementById('message').innerText = 'Please choose a valid number!';
   }
@@ -42,14 +50,20 @@ document.getElementById('numberGuess').value = '';
 document.getElementById('previousUserGuess').innerText='';
 document.querySelector('.clear').disabled = true;
 document.querySelector('.reset').disabled = true;
-randomNumber = Math.floor((Math.random() * 100) + 1);
+randomNumberVariable();
 }
 
-// setUserNumbers.addEventListener('click', function() {
-//   document.getElementById(parseInt('userMinNumber').value = var min;
-//   document.getElementById(parseInt('userMaxNumber').value = var max;
-//   playWithUserNumbers();
-// }
+function submitUserNumbers() {
+  document.getElementById('instruction').innerText = 'Make a guess between your selected numbers';
+  document.getElementById('message').innerText = '';
+  document.getElementById('numberGuess').value = '';
+  document.getElementById('previousUserGuess').innerText = '';
+  document.querySelector('.clear').disabled = true;
+  document.querySelector('.reset').disabled = true;
+  min = parseInt(document.getElementById('userMinNumber').value) || 1;
+  max = parseInt(document.getElementById('userMaxNumber').value) || 100;
+  randomNumber = randomNumberVariable();
+}
 
 //submit button needs to check that the min is less than the max
 
